487f83f3fbf988e4871e188e4424597acb0deeb9
RoshanKumar404 <roshankuar951@gmail.com>
2024-10-02 23:48:11 +0530

    used token in cookie

---
+++ utils/CookieToken.js
@@ -0,0 +1,20 @@
+const gettingTOken= require('../heloer/JwtTokens')
+
+const cookieToken=(user,res)=>{
+    const token= gettingTOken(user.id)
+    const options={
+        expires:new Date(
+            Date.now()+ 3*24*60*60*1000
+        ),
+        httpOnly:true
+    }
+    user.password= undefined
+    res.status(200).cookie('token', token ).json({
+        succes:true,
+        token,
+        user
+
+    })
+}
+
+module.exports= cookieToken;
